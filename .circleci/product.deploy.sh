3f1c63a975ec1e831f5d194426850a82c433f3d3
katoShugo <kato_shugo@cyberagent.co.jp>
2018-08-07 16:08:08 +0900

    add: file

---
+++ .circleci/product.deploy.sh
@@ -0,0 +1,12 @@
+# ACCTからサービスアカウントキーファイルを作成
+echo $ACCT_AUTH | base64 -d > ${HOME}/gcloud-service-key.json
+
+# gloudの設定
+sudo /opt/google-cloud-sdk/bin/gcloud --quiet components update
+sudo /opt/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+sudo /opt/google-cloud-sdk/bin/gcloud config set project deploy-test-212203
+
+# コンテナのUPLOAD
+docker build -t asia.gcr.io/deploy-test-212203/hello .
+sudo /opt/google-cloud-sdk/bin/gcloud docker -- push asia.gcr.io/deploy-test-212203/hello
+
